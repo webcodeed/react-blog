25ac07345b6b5d787de75804ebb88c58569b4f16
Abdullah Solahudeen <solahudeen15@gmail.com>
2024-08-22 16:01:34 +0100

    v2

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,28 +1,13 @@
-import { useState, useEffect } from "react"
 import BlogList from "./BlogList"
+import useFetch from "./useFetch"
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-
-    useEffect(() => {
-        async function fetchData(url) {
-            const res = await fetch(url)
-            const data = await res.json()
-
-            setIsPending(false)
-            setBlogs(data)
-        }
-
-        setTimeout(() => {
-            fetchData("http://localhost:3000/blogs")
-        }, 1500)
-    }, [])
-
+    const { data: blogs, isPending, error } = useFetch("http://localhost:3000/blogs")
     return (
         <>
             <div className="home">
                 {isPending && <h2>Loading....</h2>}
+                {error && <h2>Error Message: {error}</h2>}
                 {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             </div>
         </>
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchData(url) {
+            try {
+                const res = await fetch(url)
+                const data = await res.json()
+
+                setIsPending(false)
+                setData(data)
+            } catch (error) {
+                setIsPending(false)
+                setError(error.message)
+            }
+        }
+
+        setTimeout(() => {
+            fetchData(url)
+        }, 1500)
+    }, [])
+
+    return { data, isPending, error }
+}
+
+export default useFetch
